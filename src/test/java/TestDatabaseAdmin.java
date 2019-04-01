b72cd360a2579cbfda84951dad953f89fc567b59
ghr85 <ghrenfrew@gmail.com>
2019-04-01 16:38:42 +0100

    added dfunctionality to DatabaseAdmin model class

--- src/test/java/TestDatabaseAdmin.java
+++ src/test/java/TestDatabaseAdmin.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.DatabaseAdmin;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestDatabaseAdmin {
+
+    public DatabaseAdmin databaseAdmin;
+    @Before
+
+    public void before(){
+        this.databaseAdmin = new DatabaseAdmin("Rachael DonkeyWork", "JS123333",21000);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Rachael DonkeyWork", databaseAdmin.getName());
+    }
+
+
+    @Test
+    public void hasNi(){
+        assertEquals("JS123333",databaseAdmin.getNi());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        databaseAdmin.raiseSalary(1000);
+        assertEquals(22000.0,databaseAdmin.getSalary());
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(210.0,databaseAdmin.payBonus());
+    }
 }
